6bdc9141e05ece633880eec5a92645fb5370752d
reiyszw <reiyszw@gmail.com>
2026-08-31 22:56:31 +0900

    Passing Data to Components with Props

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Post from "./components/Post";
 function App() {
   return (
     <main>
-      <Post />
-      <Post />
+      <Post author="Maximilian" body="React.js is awesome!" />
+      <Post author="Manuel" body="Check out the full course" />
       <Post />
       <Post />
     </main>
