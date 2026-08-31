7fd548e26e286b3b2cad7a00b761095599da44ce
reiyszw <reiyszw@gmail.com>
2026-08-31 23:18:09 +0900

    CSS Styling & CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ function App() {
     <main>
       <Post author="Maximilian" body="React.js is awesome!" />
       <Post author="Manuel" body="Check out the full course" />
-      <Post />
-      <Post />
     </main>
   );
 }
