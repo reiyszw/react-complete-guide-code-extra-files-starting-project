a429bbc5224a869317286cccbfd31190a286c24f
reiyszw <reiyszw@gmail.com>
2026-08-31 23:32:40 +0900

    Preparing the App For State Management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostList(props) {
   return (
-    <ul className={classes.posts}>
-      {props.posts.map((post) => (
-        <Post author={post.author} body={post.body} />
-      ))}
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        {props.posts.map((post) => (
+          <Post author={post.author} body={post.body} />
+        ))}
+      </ul>
+    </>
   );
 }
 
