610ea096667ca21e3698dbd39a3d5b1849022743
reiyszw <reiyszw@gmail.com>
2026-09-01 17:15:42 +0900

    Adding Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+import "./index.css";
+import NewPost from "./components/NewPost";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  {
+    path: "/create-post",
+    element: <NewPost />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
